d3c1854f87d907abe0e90a0c83e9a3b7e7f6ebb0
Made NN <made@kata.ai>
2021-08-10 20:41:47 +0700

    Init data GLUE

---
+++ newlm/glue/configs.py
@@ -0,0 +1,44 @@
+GLUE_CONFIGS = {
+    "cola": {
+        "keys": ("sentence", None),
+        "metric_name": "matthews_correlation",
+    },
+    "mnli": {
+        "keys": ("premise", "hypothesis"),
+        "num_labels": 3,
+        "validation_key": "validation_matched",
+    },
+    "mnli-mm": {
+        "keys": ("premise", "hypothesis"),
+        "validation_key": "validation_mismatched",
+    },
+    "mrpc": {"keys": ("sentence1", "sentence2")},
+    "qnli": {"keys": ("question", "sentence")},
+    "qqp": {"keys": ("question1", "question2")},
+    "rte": {"keys": ("sentence1", "sentence2")},
+    "sst2": {"keys": ("sentence", None)},
+    "stsb": {
+        "keys": ("sentence1", "sentence2"),
+        "num_labels": 1,
+        "metric_name": "pearson",
+    },
+    "wnli": {"keys": ("sentence1", "sentence2")},
+}
+
+
+class GlueConfig:
+    def __init__(self, task: str):
+        self.__validate_task(task)
+        self.task = task
+        self.actual_task = "mnli" if task == "mnli-mm" else task
+        self.keys = GLUE_CONFIGS.get(task).get("keys")
+        self.num_labels = GLUE_CONFIGS.get(task).get("num_labels", 2)
+        self.metric_name = GLUE_CONFIGS.get(task).get("metric_name", "accuracy")
+        self.training_key = "train"
+        self.validation_key = GLUE_CONFIGS.get(task).get("validation_key", "validation")
+
+    def __validate_task(self, task: str):
+        if task not in GLUE_CONFIGS:
+            raise ValueError(
+                f"Task {task} not exist. Please check the available GLUE tasks"
+            )
