b4912d89df87f08814990c8944fe191170be0dce
Alham Fikri Aji <aji@kata.ai>
2021-08-16 03:18:43 +0000

    add sampler for EN data

---
+++ shuffle-and-sample.sh
@@ -0,0 +1,23 @@
+DATA_DIR=examples/data
+RATIO=5
+
+mkdir $DATA_DIR/text/en.$RATIO-percent
+OUTFILE=$DATA_DIR/text/en.$RATIO-percent/text.txt
+
+# compute the actual documents needed.
+TOTAL_DOCS=5216027
+N=$((RATIO*TOTAL_DOCS/100))
+
+echo "sampling $N documents"
+
+# cat the whole data, then merge documents into a single line
+# then, shuffle and select N docs
+# resplit the document into lines
+
+cat $DATA_DIR/{books,wikipedia}/* \
+	| awk '{gsub(/\n/, "~x~x~")} 1' RS= \
+	| shuf -n $N --random-source=$DATA_DIR/wikipedia/wikipedia.txt-00000-of-00500 \
+	| awk '{gsub(/~x~x~/, "\n")} 1' ORS="\n\n" > $OUTFILE
+
+echo "done = $OUTFILE"
+
