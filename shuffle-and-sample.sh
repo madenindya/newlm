41e2a9ef8564e4a4d4cb6e22b20c95b3a4f23cdf
Alham Fikri Aji <aji@kata.ai>
2021-08-18 00:43:28 +0000

    filter long words, adjust yaml to follow the original BERT config

--- shuffle-and-sample.sh
+++ shuffle-and-sample.sh
@@ -12,15 +12,18 @@ N=$((RATIO*TOTAL_DOCS/100))
 echo "sampling $N documents"
 
 # cat the whole data, then merge documents into a single line
+# filter by length
 # then, shuffle and select N docs
 # resplit the document into lines
-# remove the last line of the input and output, since it is a trailing empty spaces.
+# remove consequtive blank lines, since a document might be filtered completely.
+# remove the last line of the output, since it is a trailing empty spaces.
 
 cat $DATA_DIR/{books,wikipedia}/* \
-	| head -n -1 \
+	| awk 'NF <= 128 {print $0}' \
 	| awk '{gsub(/\n/, "~x~x~")} 1' RS= \
 	| shuf -n $N --random-source=$DATA_DIR/wikipedia/wikipedia.txt-00000-of-00500 \
 	| awk '{gsub(/~x~x~/, "\n")} 1' ORS="\n\n" \
+	| cat -s \
 	| head -n -1 \
 	| iconv -c -t utf8 > $OUTFILE
 
