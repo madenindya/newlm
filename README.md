999f786c090ee14ce52b634cc533f8ff773a6cd4
afaji <afaji321@gmail.com>
2021-08-18 08:44:54 +0700

    En sampler (#4)
    * add sampler for EN data
    * edit README
    * convert input to utf-8
    * remove extra newlines on generated file
    * filter long words, adjust yaml to follow the original BERT config
    * sample to 1%
    
    Co-authored-by: Alham Fikri Aji <aji@kata.ai>

--- README.md
+++ README.md
@@ -6,4 +6,24 @@ Run train BERT LM:
 python -m newlm run_pretrain --config_file="examples/configs/run.yaml"
 ```
 
+
+### Sample and traing English LM
+
+First, put all Wikipedia files and BookCorpus files into the following:
+```
+examples/data/wikipedia/
+examples/data/books/
+```
+
+Then, sample the data (to 1 percent)
+```
+./shuffle-and-sample.sh 1
+```
+
+Then, train
+```
+python -m newlm run_pretrain --config_file="examples/configs/run.1-percent.yaml"
+```
+
+
 Please check example file.
---
+++ shuffle-and-sample.sh
@@ -0,0 +1,31 @@
+DATA_DIR=examples/data
+RATIO=$1
+
+echo "SAMPLING TO $RATIO PERCENT"
+mkdir $DATA_DIR/text/en.$RATIO-percent
+OUTFILE=$DATA_DIR/text/en.$RATIO-percent/text.txt
+
+# compute the actual documents needed.
+TOTAL_DOCS=5216027
+N=$((RATIO*TOTAL_DOCS/100))
+
+echo "sampling $N documents"
+
+# cat the whole data, then merge documents into a single line
+# filter by length
+# then, shuffle and select N docs
+# resplit the document into lines
+# remove consequtive blank lines, since a document might be filtered completely.
+# remove the last line of the output, since it is a trailing empty spaces.
+
+cat $DATA_DIR/{books,wikipedia}/* \
+	| awk 'NF <= 128 {print $0}' \
+	| awk '{gsub(/\n/, "~x~x~")} 1' RS= \
+	| shuf -n $N --random-source=$DATA_DIR/wikipedia/wikipedia.txt-00000-of-00500 \
+	| awk '{gsub(/~x~x~/, "\n")} 1' ORS="\n\n" \
+	| cat -s \
+	| head -n -1 \
+	| iconv -c -t utf8 > $OUTFILE
+
+echo "done = $OUTFILE"
+
