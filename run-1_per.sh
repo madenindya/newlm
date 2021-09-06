ec1378bbad6211b2a289ec3e2bb84cef8e078a21
Made NN <made@kata.ai>
2021-09-06 05:09:49 +0000

    wip

--- run-1_per.sh
+++ run-1_per.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-echo "CREATE THE DATA"
-./shuffle-and-sample.sh 1
+# echo "========== CREATE THE DATA =========="
+# ./shuffle-and-sample.sh 1
 
-echo "RUN ALL (BUILD LM + BENCHMARK GLUE)"
+# echo "========== RUN ALL (BUILD LM + BENCHMARK GLUE) ========== "
+# source .env.template
+# python -m newlm run_all --config_file="examples/configs/run.1-percent.yaml"
+
+echo "========== Continue Run Glue ========== "
 source .env.template
-python -m newlm run_all --config_file="examples/configs/run.1-percent.yaml"
+python -m newlm run_glue --config_file="examples/configs/run.1-percent.glue.yaml"
