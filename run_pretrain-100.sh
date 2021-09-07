1bf6c66a0f8bb360cfd6a8b890450affd33f9cf7
Made NN <made@kata.ai>
2021-09-07 10:06:56 +0700

    Split run pretrain

--- run_pretrain-100.sh
+++ run_pretrain-100.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo ">>>>> Run all experiments"
-
 source ~/anaconda3/etc/profile.d/conda.sh # change if necessary
 conda activate newlm-py38
 conda info
@@ -9,7 +7,3 @@ conda info
 echo ">>>>> Create LM 100 Percent and Run Downstream GLUE"
 source .env.gcloud
 python -m newlm run_all --config_file="examples/configs_gcloud/run.100-percent.yaml"
-
-echo ">>>>> Create LM 50 Percent and Run Downstream GLUE"
-source .env.gcloud
-python -m newlm run_all --config_file="examples/configs_gcloud/run.50-percent.yaml"
