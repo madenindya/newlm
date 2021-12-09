ba8316c846401a54432f9adf6d89757d17dc871e
Made NN <made@kata.ai>
2021-12-09 13:01:29 +0000

    Pretify summary

--- summarize_tuning.py
+++ summarize_tuning.py
@@ -9,15 +9,19 @@ task = args[2]
 
 print(f"Summarizing {task}'s runs from dir: {output_dir}")
 
-all_summary = {}
+all_summary = []
 
-for name in glob.glob(f'{output_dir}/*/*/*/glue/{task}/all_results.json'):
-    filename = name
+for filename in glob.glob(f'{output_dir}/*/*/*/glue/{task}/all_results.json'):
     file_tokens = filename.split("/")
-    key = "-".join(file_tokens[-6:-3])
-    with open(name, "r+") as fr:
+    bs = int(file_tokens[-6].split("_")[1])
+    lr = float(file_tokens[-5].split("_")[1])
+    seed = int(file_tokens[-4].split("_")[1])
+    with open(filename, "r+") as fr:
         result = json.loads(fr.read())
-        all_summary[key] = result
+        result['bs'] = bs
+        result['lr'] = lr
+        result['seed'] = seed
+        all_summary.append(result)
 
 all_summary_str = json.dumps(all_summary, indent = 4)
 
