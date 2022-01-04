163d72143491f500b4eda15cad346a7e5f82be69
Made NN <made@kata.ai>
2022-01-04 11:40:50 +0000

    Add flip input_embeds

--- newlm/lm/bert/modeling_bert/bert_head.py
+++ newlm/lm/bert/modeling_bert/bert_head.py
@@ -3,7 +3,6 @@ from newlm.lm.elmo.modeling_elmo.elmo_utils import flip_tensor_by_length, get_se
 
 
 class BertLMHeadR2LModel(BertLMHeadModel):
-
     def __init__(self, config):
         super().__init__(config)
 
@@ -12,8 +11,9 @@ class BertLMHeadR2LModel(BertLMHeadModel):
         **bert_args,
     ):
 
-        input_ids = bert_args['input_ids']
-        labels = bert_args['labels']
+        input_ids = bert_args["input_ids"]
+        inputs_embeds = bert_args["inputs_embeds"]
+        labels = bert_args["labels"]
 
         # Flip inputs
         (batch_size, sequence_lengths) = get_sequence_lengths(
@@ -27,13 +27,20 @@ class BertLMHeadR2LModel(BertLMHeadModel):
             else None
         )
 
+        flip_inputs_embeds = (
+            flip_tensor_by_length(inputs_embeds, batch_size, sequence_lengths)
+            if inputs_embeds is not None
+            else None
+        )
+
         flip_labels = (
             flip_tensor_by_length(labels, batch_size, sequence_lengths)
             if labels is not None
             else None
         )
 
-        bert_args['input_ids'] = flip_input_ids
-        bert_args['labels'] = flip_labels
+        bert_args["input_ids"] = flip_input_ids
+        bert_args["inputs_embeds"] = flip_inputs_embeds
+        bert_args["labels"] = flip_labels
 
         return super().forward(**bert_args)
