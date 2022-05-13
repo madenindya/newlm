0dd85d0046a7712b731e9392c9be6f04b2016e6d
Made Nindyatama <made.nindyatama@gmail.com>
2022-05-13 22:06:22 +0700

    Fix bug 2-tower script finetune grid

--- run_grid-elmo-2-tower-all.sh
+++ run_grid-elmo-2-tower-all.sh
@@ -3,14 +3,14 @@
 
 echo "Run Grid Search for ELMO 2 Tower finetune-v1"
 
-./run_grid-elmo-2-tower-all.sh "cola" 0 &
-./run_grid-elmo-2-tower-all.sh "mrpc" 1 &
-./run_grid-elmo-2-tower-all.sh "rte" 2 &
-./run_grid-elmo-2-tower-all.sh "stsb" 3 &
+./run_grid-elmo-2-tower.sh "cola" 0 &
+./run_grid-elmo-2-tower.sh "mrpc" 1 &
+./run_grid-elmo-2-tower.sh "rte" 2 &
+./run_grid-elmo-2-tower.sh "stsb" 3 &
 
-./run_grid-elmo-2-tower-all.sh "mnli" 4,5 &
-./run_grid-elmo-2-tower-all.sh "qqp" 6,7 &
+./run_grid-elmo-2-tower.sh "mnli" 4,5 &
+./run_grid-elmo-2-tower.sh "qqp" 6,7 &
 
 # Uncomment & Run the following after cola, mrpc, rte, stsb finished
-# ./run_grid-elmo-2-tower-all.sh "qnli" 0,1 &
-# ./run_grid-elmo-2-tower-all.sh "sst2" 2,3 &
+# ./run_grid-elmo-2-tower.sh "qnli" 0,1 &
+# ./run_grid-elmo-2-tower.sh "sst2" 2,3 &
