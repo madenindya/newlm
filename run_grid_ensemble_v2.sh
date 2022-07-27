69035f6e1846f62f8edf8f3e704b8b07f9fb7be5
Made NN <made@kata.ai>
2022-07-27 11:32:57 +0000

    Update bak

--- run_grid_ensemble_v2.sh
+++ run_grid_ensemble_v2.sh
@@ -14,5 +14,5 @@ tasks=( cola mnli mrpc qnli qqp rte sst2 stsb )
 
 for task in "${tasks[@]}"
 do
-    python summarize_ensemble.py /mnt/data4/made_workspace/newlm-output/best_l2r_r2l_finetuned/ens-l2r_4e-4-l2r_2e-4-test $task
+    python summarize_ensemble.py /mnt/data4/made_workspace/newlm-output/best_l2r_r2l_finetuned/ens-l2r_4e-4-l2r_2e-4 $task
 done
